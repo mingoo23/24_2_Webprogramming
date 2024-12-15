a2633591b6f8ec9405aed03ef321d8bd5669517a
차민우 <chatang9876@gmail.com>
2024-12-16 03:46:30 +0900

    탭 문제 해결

--- SharePlaylists/src/main/webapp/scripts.js
+++ SharePlaylists/src/main/webapp/scripts.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    const cache = {}; // 데이터를 저장할 캐시 객체
-
     // 초기설정 - 활성화된 탭 콘텐츠 로드
     const defaultFile = $(".tab-header.active").data("file");
     loadContent(defaultFile);
@@ -20,34 +18,16 @@ $(document).ready(function () {
 
     // 콘텐츠를 동적으로 로드하는 함수
     function loadContent(file) {
-        // 캐시에 데이터가 있다면 바로 사용
-        if (cache[file]) {
-            console.log(`캐시에서 로드: ${file}`);
-            $("#main-content").html(cache[file]);
-            
-            // 기존 콘텐츠를 비활성화하고 새 콘텐츠 삽입
-            $("#main-content").empty().html(cache[file]).find(".tab-content").addClass("active");
-        } else {
-            // AJAX 요청으로 데이터 가져오기
-            console.log(`서버에서 로드: ${file}`);
-            $("#main-content").load(file, function (response, status, xhr) {
-                if (status === "success") {
-                    // 성공적으로 로드하면 캐시에 저장
-                    cache[file] = response;
-                    // 기존 콘텐츠를 비활성화하고 새 콘텐츠 삽입
-                    $("#main-content").empty().html(response).find(".tab-content").addClass("active");
-                } else if (status === "error") {
-                    console.error("콘텐츠를 로드하는 중 오류 발생:", xhr.status, xhr.statusText);
-                    $("#main-content").html("<p>콘텐츠를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.</p>");
-                }
-            });
-        }
+        console.log(`서버에서 로드: ${file}`);
+        // AJAX 요청으로 데이터 가져오기
+        $("#main-content").load(file, function (response, status, xhr) {
+            if (status === "success") {
+                // 성공적으로 로드한 경우 콘텐츠 삽입
+                $("#main-content").empty().html(response).find(".tab-content").addClass("active");
+            } else if (status === "error") {
+                console.error("콘텐츠를 로드하는 중 오류 발생:", xhr.status, xhr.statusText);
+                $("#main-content").html("<p>콘텐츠를 불러오지 못했습니다. 잠시 후 다시 시도해주세요.</p>");
+            }
+        });
     }
 });
-
-//hmtl에 id="clear-cache-btn" 버튼 추가해 캐시 초기화 테스트에 사용
-$("#clear-cache-btn").click(function () {
-    cache = {}; // 캐시 초기화
-    console.log("캐시가 초기화되었습니다.");
-});
-
--- SharePlaylists/src/main/webapp/workspace/workspace.jsp
+++ SharePlaylists/src/main/webapp/workspace/workspace.jsp
@@ -10,7 +10,6 @@
     <a href="#workspace-social" class="workspace-header" data-file="./workspace/workspace_social.html" >친구</a
     >
   </div>
-  <button id="clear-cache-btn">캐시 초기화</button>
 
   <div id="workspace-content">
   </div>
